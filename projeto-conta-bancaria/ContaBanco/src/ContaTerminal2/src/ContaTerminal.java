1968c971a2548d3b43d42cb62c7f9df319562316
Renan <renan.economia.2011@gmail.com>
2024-05-12 21:50:57 -0400

    Exclusão de pastas não usadas

--- projeto-conta-bancaria/ContaBanco/src/ContaTerminal2/src/ContaTerminal.java
+++
@@ -1,9 +0,0 @@
-public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-       int Numero = Integer.args[0];
-       String Agencia = args [1];
-       String NomeCliente = args [3];
-       float Saldo = float.args[4];
-    
-    }
-}
