62af8f70bdd6f043771624a4376e5f4b3ac9627e
Renan <renan.economia.2011@gmail.com>
2024-05-12 21:41:03 -0400

    Criação da Conta e do Usuario

---
+++ projeto-conta-bancaria/ContaBanco/src/ContaTerminal2/src/Usuario.java
@@ -0,0 +1,14 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+    
+        new int NumeroConta = int 1021;
+        new String Agencia = "067-8";
+        new String NomeCliente = "MARIO ANDRADE";
+        new float SaldoConta = 237.48;
+
+        System.out.println(NumeroConta);
+    
+
+
+    }
+}
