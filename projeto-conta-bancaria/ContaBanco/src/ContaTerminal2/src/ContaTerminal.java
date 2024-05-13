5560a5bcf7c95c043db474f5931568c761edf719
Renan <renan.economia.2011@gmail.com>
2024-05-12 21:44:18 -0400

    Adição das []

--- projeto-conta-bancaria/ContaBanco/src/ContaTerminal2/src/ContaTerminal.java
+++ projeto-conta-bancaria/ContaBanco/src/ContaTerminal2/src/ContaTerminal.java
@@ -1,5 +1,9 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       int Numero = Integer.args[0];
+       String Agencia = args [1];
+       String NomeCliente = args [3];
+       float Saldo = float.args[4];
+    
     }
 }
