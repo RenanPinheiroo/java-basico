530388795061c5878d896f83bbe203a8ead597eb
Renan <renan.economia.2011@gmail.com>
2024-05-29 19:47:45 -0400

    atualização

--- poo-escola/src/Escola/Aluno.java
+++ poo-escola/src/Escola/Aluno.java
@@ -1,3 +1,5 @@
+package Escola;
+
 public class Aluno {
 	String nome;
 	int idade;
--- poo-escola/src/Escola/Escola.java
+++ poo-escola/src/Escola/Escola.java
@@ -1,3 +1,5 @@
+package Escola;
+
 public class Escola {
 	public static void main(String[] args) {
 		Aluno felipe = new Aluno();
